57cf53d0d209a050d52bdd8ea600768decda7dcb
Anders Lund <61545005+AnderzL7@users.noreply.github.com>
2025-04-09 00:58:15 +0200

    Fixes build error for Suspense around useSearchParams

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import LoginComponent from "@components/auth/LoginComponent";
 import { Container } from "@mui/material";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
@@ -14,7 +15,9 @@ export default function LoginPage() {
         padding: 3,
       }}
     >
-      <LoginComponent onLoginSuccess="redirect" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginComponent onLoginSuccess="redirect" />
+      </Suspense>
     </Container>
   );
 }
